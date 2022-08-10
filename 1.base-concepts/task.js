1bb573a7296cb23369f461dee97baac24b19d2a8
Centurion <ajlykard@mail.ru>
2022-08-10 18:03:19 +0300

    The first task

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,5 +1,13 @@
 function solveEquation(a, b, c) {
-  let arr;
+  "use strict";
+  let arr = [];
+  let D = Math.pow(b, 2) - 4 * a * c;
+  if (D > 0) {
+    arr.push((-b + Math.sqrt(D)) / (2 * a));
+    arr.push((-b - Math.sqrt(D)) / (2 * a));
+  } else if (D == 0) {
+    arr.push(-b / (2 * a));
+  }
   // код для задачи №1 писать здесь
   return arr; // array
 }
